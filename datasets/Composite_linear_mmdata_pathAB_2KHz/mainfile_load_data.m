029cfc7a3697a985aa3b2437b0113b2022d1855a
SamuelBFG <samuelbfgomes@gmail.com>
2023-03-23 12:03:08 -0300

    include composite datasets

---
+++ datasets/Composite_linear_mmdata_pathAB_2KHz/mainfile_load_data.m
@@ -0,0 +1,12 @@
+clc
+clear
+
+cdata_AP1_pathAB_linear = load('pathAB_composite_linear_AP1_downsampled2Khz_win100.txt');
+cdata_AP2_pathAB_linear = load('pathAB_composite_linear_AP2_downsampled2Khz_win100.txt');
+cdata_AP3_pathAB_linear = load('pathAB_composite_linear_AP3_downsampled2Khz_win100.txt');
+cdata_AP4_pathAB_linear = load('pathAB_composite_linear_AP4_downsampled2Khz_win100.txt');
+cdata_AP5_pathAB_linear = load('pathAB_composite_linear_AP5_downsampled2Khz_win100.txt');
+cdata_AP6_pathAB_linear = load('pathAB_composite_linear_AP6_downsampled2Khz_win100.txt');
+cdata_AP7_pathAB_linear = load('pathAB_composite_linear_AP7_downsampled2Khz_win100.txt');
+cdata_AP8_pathAB_linear = load('pathAB_composite_linear_AP8_downsampled2Khz_win100.txt');
+cdata_AP9_pathAB_linear = load('pathAB_composite_linear_AP9_downsampled2Khz_win100.txt');
---
+++ datasets/Composite_linear_mmdata_pathBA_2KHz/mainfile_load_data.m
@@ -0,0 +1,12 @@
+clc
+clear
+
+cdata_AP1_pathBA_linear = load('pathBA_composite_linear_AP1_downsampled2Khz_win100.txt');
+cdata_AP2_pathBA_linear = load('pathBA_composite_linear_AP2_downsampled2Khz_win100.txt');
+cdata_AP3_pathBA_linear = load('pathBA_composite_linear_AP3_downsampled2Khz_win100.txt');
+cdata_AP4_pathBA_linear = load('pathBA_composite_linear_AP4_downsampled2Khz_win100.txt');
+cdata_AP5_pathBA_linear = load('pathBA_composite_linear_AP5_downsampled2Khz_win100.txt');
+cdata_AP6_pathBA_linear = load('pathBA_composite_linear_AP6_downsampled2Khz_win100.txt');
+cdata_AP7_pathBA_linear = load('pathBA_composite_linear_AP7_downsampled2Khz_win100.txt');
+cdata_AP8_pathBA_linear = load('pathBA_composite_linear_AP8_downsampled2Khz_win100.txt');
+cdata_AP9_pathBA_linear = load('pathBA_composite_linear_AP9_downsampled2Khz_win100.txt');
